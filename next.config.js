2248935e41f25d1b539a9703afab02f501b5bb45
Lauren Dorman <hello@laurendorman.io>
2018-02-08 12:02:59 +0100

    add export path maps

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ const { default: config } = require('nextein/config');
 
 module.exports = config({
   exportPathMap: () => ({
+    '/': { page: '/' },
+    '/about': { page: '/about' },
+    '/colophon': { page: '/colophon' },
+    '/projects': { page: '/projects' },
+    '/style-guide': { page: '/style-guide' },
     '/work': { page: '/projects' },
+    '/writing': { page: '/writing' },
   }),
 });
