a3b9428ae30c0bbf3c4ea22241d08f5ecb10da85
Lauren Dorman <hello@laurendorman.io>
2018-02-10 12:50:26 +0100

    update style guide rendering, remove anchors

--- pages/style-guide.js
+++ pages/style-guide.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MarkdownWrapper from '../components/MarkdownWrapper';
+import Markdown from '../components/Markdown';
 
 const data = {
   title: 'Style Guide',
@@ -28,27 +28,27 @@ This Style Guide serves as a very opinionated and intentional guide to how I dev
     1. [Properties and Values](#properties-and-values)
     1. [Browser Prefixes](#browser-prefixes)
     1. [Selectors](#selectors)
-    1. [Pseudo-elements and Pseudo-classes](#pseudo-elements-and-pseudo-classes)
+    1. [Pseudo-elements](#pseudo-elements)
 4. [Inspiration](#inspiration)
 
-## <a name='doctype'>HTML5</a>
+## HTML5
 The use of HTML5 should strictly follow the [HTML5 W3 Specifications](http://www.w3.org/TR/html5/). For browser support, use [CanIUse](http://caniuse.com/#cats=HTML5).
 
-### <a name='doctype'>Doctype</a>
+### Doctype
 Enforce standards mode and more consistent rendering in every browser by declaring the HTML5 Doctype at the beginning of every HTML page.
 
 ~~~html
 <!DOCTYPE html>
 ~~~
 
-### <a name='writing-html-syntax'>Writing HTML Syntax</a>
+### Writing HTML Syntax
 - Use a soft tab (two spaces), so that the HTML is rendered the same across browsers.
 - Nested elements should be indented once per level.
 - When an attribute requires it, use single quotes unless for content.
 - Use lowercase and dashes, not underscore or camelCase.
 - Do not include trailing slash in self-closing elements.
 
-### <a name='spacing'>Spacing</a>
+### Spacing
 Large sections of HTML should be separated by a one line break for readability.
 
 ~~~html
@@ -63,17 +63,17 @@ Large sections of HTML should be separated by a one line break for readability.
 </main>
 ~~~
 
-### <a name='urls'>URLs</a>
+### URLs
 Use relative URLs whenever possible; this will prevent problems if the base URL ever changes during a project's lifespan.
 
 ~~~html
 <a href='/about' title="About">About</a>
 ~~~
 
-## <a name='sass'>Sass</a>
-[Sass](http://sass-lang.com/) is my preferred choice for CSS preprocessing, it allows the author to add variables, nesting, functions, and mixins to their stylesheets. This document uses Sass syntax.
+## Sass
+[Sass](http://sass-lang.com/) is my preferred choice for CSS processing, it allows the author to add variables, nesting, functions, and mixins to their stylesheets. This document uses Sass syntax.
 
-### <a name='variables'>Variables</a>
+### Variables
 
 Write global variables just as you would a CSS declaration, but optionally include one line break and comments between related variables for grouping and readability.
 
@@ -112,17 +112,17 @@ When importing, do not include the ~.scss~ file extension or prepending undersco
 @import 'utilities/variables';
 ~~~
 
-### <a name='functions'>Functions</a>
+### Functions
 ~~~
 Documentation in progress.
 ~~~
 
-### <a name='mixins'>Mixins</a>
+### Mixins
 ~~~
 Documentation in progress.
 ~~~
 
-### <a name='nesting'>Nesting</a>
+### Nesting
 Avoid nesting too deeply Sass for the sake of it. Things can get pretty ugly, pretty quickly.
 
 ~~~scss
@@ -150,39 +150,39 @@ The result will not be good.
 
 Do not nest more than 3 levels deep.
 
-### <a name='class-names'>Class Names</a>
+### Class Names
 - Use the BEM (block, element, modifier) methodology for naming things, i.e., ~btn__text--sold-out~.
 - Be specific when naming classes. Use meaningful names that describe the action or functionality of a class rather than is appearance, i.e., ~.btn--danger~ instead of ~.btn--red~.
 - Abbreviate some class names, but make sure anyone can understand the class, i.e., ~.cta~ instead of ~.call-to-action~.
 - Prefix classes based on the closest parent element or base class.
 
-## <a name='css'>CSS</a>
+## CSS
 The use of CSS should strictly follow the [CSS W3 Specifications](http://www.w3.org/TR/CSS/) for CSS properties and values. For browser support, use [CanIUse](http://caniuse.com/#cats=CSS).
 
-### <a name='writing-css-syntax'>Writing CSS Syntax</a>
+### Writing CSS Syntax
 - For readability, use a soft tab (two spaces).
 - Nested elements (in Sass) should be indented with a soft tab.
 - Include one space after the ~:~ for each declaration.
 - Each individual declaration should be written on its own line and indented with a soft tab.
 - End each declaration with a semi-colon.
 
-### <a name='properties-and-values'>Properties and Values</a>
+### Properties and Values
 - Avoid specifying units for zero values, i.e., ~margin: 0;~ instead of ~margin: 0px~;
 - Use shorthand whenever possible, i.e., ~font~ instead of ~font-size~ and ~font-family~ – be very intentional with typography.
 - Use ~!important~ when necessary, but this should be avoided.
 - Use single quotes and never double quotes.
 - Stay consistent with units, i.e., do not use a mixture of ~rem~ and ~px~ for typography, unless for browser support where a ~rem~ fall back is needed.
 
-### <a name='browser-prefixes'>Browser Prefixes</a>
+### Browser Prefixes
 Figuring out which properties require browser prefixes is a hefty task, but [CanIUse](http://caniuse.com) provides information about browser support and which properties require prefixes. I recommend using [Autoprefixer](https://github.com/postcss/autoprefixer) if you are comfortable using a task runner.
 
-### <a name='selectors'>Selectors</a>
+### Selectors
 - Class selectors should be used before ID selectors.
 - The universal ~*~ selector should be avoided at all costs.
 - Avoid the use of multiple type selectors (also known as descendant selectors) whenever possible, i.e., ~ul li a~.
 - If selectors use the same declarations, group them together, i.e., ~h1, h2, h3, h4, h5, h6 { ... }~.
 
-### <a name='pseudo-elements-and-pseudo-classes'>Pseudo-elements and Pseudo-classes</a>
+### Pseudo-elements
 When grouping pseudo-elements and classes with other selectors, include after the element they affect.
 
 ~~~css
@@ -201,8 +201,8 @@ h1::after {
 }
 ~~~
 
-## <a name='inspiration'>Inspiration</a>
+## Inspiration
 This document is inspired by my personal writing style, [Code Guide by @mdo](http://codeguide.co/), [Google's Styleguide](https://github.com/google/styleguide), and [CSS Guidelines](http://cssguidelin.es/).
 `;
 
-export default () => <MarkdownWrapper data={data}>{content}</MarkdownWrapper>;
+export default () => <Markdown data={data}>{content}</Markdown>;
